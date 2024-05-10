897ac7e89a20b87c85f3017592926340ae82f55d
Seamoon Pandey <hackedasocial@gmail.com>
2024-05-10 19:35:17 +0545

    add the review section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check, Star } from "lucide-react";
 import Phone from "@/components/Phone";
+import { Icons } from "@/components/Icons";
+import Reviews from "@/components/Reviews";
 export default function Home() {
   return (
     <div className="bg-slate-50">
@@ -92,6 +94,76 @@ export default function Home() {
           </div>
         </MaxWidthWrapper>
       </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center ga[-16 sm:gap-32">
+            <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+                <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight  font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2">customers <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" /> </span>say</h2>
+            <Image src="/snake-2.png" className="w-24 order-0 lg:order-2" alt="snake"  height={1000} width={1000}/>
+            </div>
+            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+                <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                    <div className="flex gap-0.5 mb-2">
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                    </div>
+                    <div className="text-lg leading-8">
+                        <p>
+                            &quot;The case feels durable and the print quality is amazing. I&apos;ve had mine for a year now and it still looks brand new. <span className="p-0.5 bg-slate-800 text-white">The image looks super clean</span>, on the case&quot;
+                        </p>
+                    </div>
+                    <div className="flex gap-4 mt-2">
+                        <Image src='/users/user-1.png' alt='user' className="rounded-full h-12 w-12 object-cover" height={1000} width={1000} />
+                        <div className="flex flex-col">
+                            <p className="font-semibold">
+                                Jonathan
+                            </p>
+                            <div className="flex gap-1.5 items-center text-zinc-600">
+                                <Check className="h-4 w-4  stroke-[3px] text-green-600"/>
+                                <p className="text-sm">Verified Purchase</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+                <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+                    <div className="flex gap-0.5 mb-2">
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                        <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                    </div>
+                    <div className="text-lg leading-8">
+                        <p>
+                            &quot;All the other cases I had got heavy scratches easily by my key but this case is awesome. I&apos;ve had mine for a month now but its still clean.&quot;
+                        </p>
+                    </div>
+                    <div className="flex gap-4 mt-2">
+                        <Image src='/users/user-4.jpg' alt='user' className="rounded-full h-12 w-12 object-cover" height={1000} width={1000} />
+                        <div className="flex flex-col">
+                            <p className="font-semibold">
+                                Hace
+                            </p>
+                            <div className="flex gap-1.5 items-center text-zinc-600">
+                                <Check className="h-4 w-4  stroke-[3px] text-green-600"/>
+                                <p className="text-sm">Verified Purchase</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </MaxWidthWrapper>
+        <div className="pt-16">
+            <Reviews />
+        </div>
+      </section>
+
     </div>
   );
 }
