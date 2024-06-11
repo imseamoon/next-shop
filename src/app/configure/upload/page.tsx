e037f056acc28d402e83b44d82e91f8bead15b72
Seamoon Pandey <hackedasocial@gmail.com>
2024-06-11 13:01:05 +0545

    setup layout for full screen drag

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
 
 
   return (
-    <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-tray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",{
+    <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",{
         "ring-blue-900/25 bg-blue-900/10": isDragOver
     })}>
 
@@ -33,7 +33,7 @@ const Page = () => {
             >
                 {
                     ({getRootProps, getInputProps})=>(
-                        <div className="h-full w-full flex flex-col flex-1 items-center justify-center" {...getRootProps()}>
+                        <div className="h-full w-full flex-1 flex flex-col items-center justify-center" {...getRootProps()}>
                             <input {...getInputProps()} />
                           
                         </div>
